edec3490668a0e4e26a288d5e41db65747bd1f3a
Kyle Jones <mynameiskyle242@gmail.com>
2022-07-23 18:13:09 -0400

    Initial commit

---
+++ src/test/java/net/kylejones/workouttracker/WorkouttrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kylejones.workouttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkouttrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
