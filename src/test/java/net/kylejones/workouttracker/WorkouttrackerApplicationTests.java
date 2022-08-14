de248120a940891492b2a288811d6545f89e7db4
Kyle Jones <mynameiskyle242@gmail.com>
2022-08-14 16:29:47 -0400

    Add integration tests for basic controller

--- src/test/java/net/kylejones/workouttracker/WorkouttrackerApplicationTests.java
+++ src/test/java/net/kylejones/workouttracker/WorkouttrackerApplicationTests.java
@@ -1,13 +1,53 @@
 package net.kylejones.workouttracker;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class WorkouttrackerApplicationTests {
+
+    @Autowired
+    private WebApplicationContext context;
+    
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    @WithMockUser
+    void testUserCanSeeMainPage() throws Exception {
+        this.mvc.perform(get("/main"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello!")));
+    }
+
+    @Test
+    void testInvalidUserCannotSeeMainPage() throws Exception {
+        this.mvc.perform(get("/main"))
+                .andExpect(status().is3xxRedirection());
+    }
 }
